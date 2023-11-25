74d4499cb3841f1602805be82d9e045c1e25f65c
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-11-26 00:38:48 +0530

    Add files via upload

---
+++ props_in_react/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+root.render(
+  <div>
+    <App/>
+  </div>,
+  document.getElementById("root")
+);
+
+
