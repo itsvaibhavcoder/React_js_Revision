4dc750ad8bc7372deff5f4e3a78df4ec4b363780
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-11-30 22:31:42 +0530

    Add files via upload

---
+++ emoji_pedia/src/emojipedia.jsx
@@ -0,0 +1,40 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning: "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning: "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning: "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "😍",
+      name: "Smiling Face with Heart-Eyes",
+      meaning: "Expresses a range of positive, affectionate feelings, especially being in love, having a crush, or appreciating something desirable."
+    },
+    {
+      id: 5,
+      emoji: "🌟",
+      name: "Glowing Star",
+      meaning: "Represents excitement, success, and admiration. Can also symbolize talent, good luck, and special achievements."
+    },
+    {
+      id: 6,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning: "Used to convey a sense of questioning, deep thought, or pondering about a particular issue, idea, or decision."
+    }
+    // Add more objects as needed...
+  ];
+export default emojipedia;  
